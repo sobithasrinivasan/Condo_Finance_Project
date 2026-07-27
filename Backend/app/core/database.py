7d630629fa423c4301e55ae1e16f4ec503605ec9
greeshmanth-aieton <greeshmanth.reddy@aietonlabs.com>
2026-07-27 20:42:30 +0530

    Add vendor module changes

--- Backend/app/core/database.py
+++ Backend/app/core/database.py
@@ -1,45 +1,6 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-
+import mysql.connector
 from app.core.settings import settings
 
-
-from urllib.parse import quote_plus
-
-DATABASE_URL = (
-    f"mysql+pymysql://{settings.DB_USER}:"
-    f"{quote_plus(settings.DB_PASSWORD)}@"
-    f"{settings.DB_HOST}:"
-    f"{settings.DB_PORT}/"
-    f"{settings.DB_NAME}"
-)
-print(
-    f"URL = mysql+pymysql://{settings.DB_USER}:*****@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
-)
-
-engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True
-)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
-Base = declarative_base()
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-import mysql.connector
 
 def check_db_connection():
     db = mysql.connector.connect(
@@ -50,6 +11,7 @@ def check_db_connection():
         database=settings.DB_NAME
     )
     db.close()
+
 
 def get_db_connection():
     return mysql.connector.connect(
